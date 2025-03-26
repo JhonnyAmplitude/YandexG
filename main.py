db540b48cd63591aefcf21c12ddb894803c031b8
JhonnyAmplitude <jhonnyamplituda@gmail.com>
2025-03-26 09:37:16 +0500

    feat: add user_router / refactor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-from auth import router as auth_router
+from Users.router import router as user_router
 
 app = FastAPI()
 
-app.include_router(auth_router)
+app.include_router(user_router)
